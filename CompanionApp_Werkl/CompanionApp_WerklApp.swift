5e4c833a1537f3f86c9bbcd2ecf8ab3a5a679a2d
Matthias <matthias.werkl@stud.fh-campuswien.ac.at>
2024-12-19 18:16:47 +0100

    Initial Commit

---
+++ CompanionApp_Werkl/CompanionApp_WerklApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionApp_WerklApp.swift
+//  CompanionApp_Werkl
+//
+//  Created by Matthias Werkl on 19.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CompanionApp_WerklApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompanionApp_WerklTests/CompanionApp_WerklTests.swift
@@ -0,0 +1,17 @@
+//
+//  CompanionApp_WerklTests.swift
+//  CompanionApp_WerklTests
+//
+//  Created by Matthias Werkl on 19.12.24.
+//
+
+import Testing
+@testable import CompanionApp_Werkl
+
+struct CompanionApp_WerklTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
